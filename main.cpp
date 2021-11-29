7486897e8cb5a3e2a650e705b16fafa78656786d
Amos-L <amosluo108@gmail.com>
2021-11-29 16:22:09 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,48 +3,19 @@
  *  Copyright 2021 Amos Luo
  */
 
-#include "std_lib_facilities.h"
+#include <iostream>
+using namespace std;
 
-int main() {
+int main()
+{
+    int input = 0;
+    cout << "Enter intger: ";
+    cin >> input;
 
-	string operation;
-	double num1;
-	double num2;
-	double result{};
-	int x = 1;
-
-	while (x = 1) {
-
-		cout << "Enter operation: \n";
-		cin >> operation;
-		cout << "Enter first operand: \n";
-		cin >> num1;
-		cout << "Enter second operand: \n";
-		cin >> num2;
-
-		if (operation == "+" || operation == "add") {
-			result = num1 + num2;
-			cout << "Result is " << result << "\n";
-		}
-		if (operation == "-" || operation == "minus") {
-			result = num1 - num2;
-			cout << "Result is " << result << "\n";
-		}
-		if (operation == "*" || operation == "mul") {
-			result = num1 * num2;
-			cout << "Result is " << result << "\n";
-		}
-		if (operation == "/" || operation == "div") {
-
-			result = num1 / num2;
-
-			if (num1 == 0 || num2 == 0) {
-				cout << "Cannot divide by zero";
-				exit;
-			}
-
-			cout << "Result is " << result << "\n";
-		}
-
-	}
+    if (input % 2 == 0) {
+        cout << "The value " << input << " is an even number";
+    }
+    if (input % 2 == 1) {
+        cout << "The value " << input << " is an odd number";
+    }
 }
